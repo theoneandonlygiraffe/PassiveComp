a5f8d76c7e6d9e8785b44d3c1ad6a82b0c4641fb
theoneandonlygiraffe <theoneandonlygiraffe@web.de>
2024-09-19 21:59:05 +0200

    implemented calculateSeries in numpy without optimisations

--- PassiveComp/core.py
+++ PassiveComp/core.py
@@ -1,12 +1,14 @@
 import math
+import numpy as np
+
 
 def createSeries(n):
     values = []
-    for m in range(0 , n):
-        k = math.pow(math.pow(10,m),1/n)
-        #k = math.ceil(k * 100) / 100
+    for m in range(0, n):
+        k = math.pow(math.pow(10, m), 1 / n)
+        # k = math.ceil(k * 100) / 100
         if n <= 24:
-            k = round(k,1)
+            k = round(k, 1)
             match k:
                 case 2.6:
                     k = 2.7
@@ -25,12 +27,13 @@ def createSeries(n):
                 case 8.3:
                     k = 8.2
         else:
-            k = round(k,2)
+            k = round(k, 2)
             if k == 8.3:
                 k = 8.2
-        
+
         values.append(k)
     return values
+
 
 def norminalizeValue(value):
     factor = 1
@@ -42,25 +45,53 @@ def norminalizeValue(value):
         factor = 100
     return value, factor
 
+
 def denorminalizeValue(value, factor):
     return value * factor
+
 
 def getNextClosestValue(list, value):
     buff = []
     for i in list:
-        buff.append(abs(i-value))
+        buff.append(abs(i - value))
     return buff.index(min(buff))
 
-def calculateSeries(list, value):
-    buff = []
-    for i in range(0, len(list)):
-        for j in range(i, len(list)):
-            total = list[i] + list[j]
-            total = round(total, 1)
-            buff.append((list[i], list[j], total))
 
-    out = getNextClosestValue([x[2] for x in buff], value)
-    return buff[out]
+def calculateSeries(list2, value):
+    """
+    caculates the best combination of values from list to archive value with value = r1 + r2
+    returns a touple with length 3. The 2 resistor values and achieved Resistance
+    """
+    # todo : minimize result matrix, add zero and infinity
+    #
+    list = np.array(list2)
+
+    # calc (minimized) result matrix
+    horizontal = np.tile(list, (list.size, 1))
+    vertical = np.transpose(horizontal)
+
+    resultMatrix = np.add(horizontal, vertical)
+
+    # calc error matrix
+    targetMatrix = np.full(resultMatrix.shape, value)
+
+    errorMatrix = np.abs(value - targetMatrix)
+
+    # get coordinates of min error
+    xCords, yCords = np.where(errorMatrix == errorMatrix.min())
+    # first coordinates are enough atm
+    x = xCords[0]
+    y = yCords[0]
+
+    # translate coordinates to values
+    xVal = list[x]
+    yVal = list[y]
+    resultVal = resultMatrix[x, y]
+
+    # for x in range(xCords.size):
+    # print(list[xCords[x]], list[yCords[x]])
+    return (xVal, yVal, resultVal)
+
 
 def calculateParallel(list, value):
     buff = []
@@ -73,5 +104,6 @@ def calculateParallel(list, value):
     out = getNextClosestValue([x[2] for x in buff], value)
     return buff[out]
 
+
 def calculateError(target, value):
-    return (((value - target) / target) * 100)
+    return ((value - target) / target) * 100
